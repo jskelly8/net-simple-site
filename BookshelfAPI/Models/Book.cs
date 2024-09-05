1c64b3c80f7572caf8ee6f8923bf0c2c21aa4185
jskelly8 <jessskelton888@gmail.com>
2024-09-05 16:27:02 -0500

    Add Book model

---
+++ BookshelfAPI/Models/Book.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookshelfAPI.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        [StringLength(13)]
+        public string ISBN { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; }
+
+        [StringLength(50)]
+        public string Genre { get; set; }
+    }
+}
